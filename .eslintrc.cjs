3cc907f53c75f9ca4b2c64b81c5392435020393b
Travis Horn <travis@travishorn.com>
2024-03-18 20:40:18 -0500

    Set up ESLint and Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+};
