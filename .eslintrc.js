b29f4045723f5326284dc1df49243f7cb0d1bd93
Travis Horn <travis@travishorn.com>
2024-03-18 20:24:01 -0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "env": {
+    "es2021": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+}
